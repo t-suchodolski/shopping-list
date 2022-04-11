a4a9dadb63c0a924b772ed948e3b256c4e6a9c0b
de-suo-faciendo-curavit <80784777+de-suo-faciendo-curavit@users.noreply.github.com>
2022-04-11 15:09:52 +0200

    work on gui, menu from scratch def -> done work still in progress

--- gui.py
+++ gui.py
@@ -1,63 +1,45 @@
-
 import PySimpleGUI as sg
+import json
 
-event, values = sg.Window('CACS Reporting Tool').Layout([[sg.Text('Select the file you wish to load')],
-                                                   [sg.Button('YES'),sg.CloseButton('NO')]]).Read()
-
+window = sg.Window('CACS Reporting Tool').Layout([[sg.Text('Add recipe?')],
+                                                   [sg.Button('YES'),sg.CloseButton('NO')]])
+event, values = window.read()
+window.close()
 
 
 def menu_scratch():
-    layout = [[sg.Text('My one-shot window.')],
+    layout = [[sg.Text('What is it?')],
               [sg.InputText()],
               [sg.Submit(), sg.Cancel()]]
 
-    window = sg.Window('Window Title', layout)
+    window = sg.Window('Recipe Creator', layout)
 
     event, values = window.read()
     window.close()
     key2 = values[0].capitalize()
-    print(key2)
     window.close()
     values2 = []
     recipe = {key2: values2}
 
-    layout = [[sg.Text('Your typed chars appear here:'), sg.Text(size=(15, 1), key='-OUTPUT-')],
-              [sg.Input(key='-IN-')],
-              [sg.Button('Show'), sg.Button('Exit')]]
+    layout = [[sg.Text('Type in ingredient here:'), sg.Text(size=(15, 1), key='-OUTPUT-')],
+              [sg.Input(key='-IN-', do_not_clear=False)],
+              [sg.Button('Add'), sg.Button('Exit')]]
 
-    window = sg.Window('Pattern 2B', layout)
+    window = sg.Window('Recipe Creator', layout)
 
     while True:
-        event, values1 = window.read()
-        if event == 'Show':
-            recipe.setdefault(key2,[]).append(values1)
-            window.close()
+        event, ing_values = window.read()
+        if event == 'Add':
+            ing_vals = list(ing_values.values())
+            ingredient = ing_vals[0].capitalize()
+            recipe.setdefault(key2,[]).append(ingredient)
+            continue
 
         if event == sg.WIN_CLOSED or event == 'Exit':
             print(recipe)
+            window.close()
             break
-
-
-    while True:
-        ans2 = input("Add recipe? Y/N ").capitalize()
-        if ans2 == 'Y':
-            key2 = input("What is it? ").capitalize()
-            values2 = []
-            recipe[key2] = values2
-
-            while True:
-                values2 = input("Add ingredient. Type Done to save: ").capitalize()
-                if values2 != "Done":
-                    recipe.setdefault(key2,[]).append(values2)
-                    continue
-                else:
-                    break
-            continue
-        elif ans2 == 'N':
-            break
-        else:
-            print('Error, try again')
-            continue
+    window.close()
 
 
     with open("menu.json", "a+") as file:
@@ -66,5 +48,10 @@ def menu_scratch():
 
 if event == 'YES':
     menu_scratch()
+
+window.close()
+
+
+
 
 
